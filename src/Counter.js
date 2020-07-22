b2816616767c2ce4e42ef52dcd1db9d5277ad9cb
Cankat Örüm <52240938+JohnCut@users.noreply.github.com>
2020-07-22 20:00:48 +0300

    States and useState func.
    - with boolean values.

--- src/Counter.js
+++ src/Counter.js
@@ -3,10 +3,13 @@ import React, { useState } from "react";
 function Counter() {
   // first value, second function
   const [count, setCount] = useState(0);
+  const [isHappy, setIsHappy] = useState(false);
   return (
     <div>
       <h2>{count}</h2>
       <button onClick={() => setCount(count + 1)}>+1</button>
+      <h3>{isHappy ? ":)" : ":("}</h3>
+      <button onClick={() => setIsHappy(!isHappy)}>Change my mood</button>
     </div>
   );
 }
